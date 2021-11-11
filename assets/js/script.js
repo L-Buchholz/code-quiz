ffa965068a548c1c44b9575dcecbddd93f959629
Lauren Buchholz <l.buchholz1988@gmail.com>
2021-11-11 16:08:54 -0700

    Currently frustrated!!

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,11 +5,22 @@ var cardEl = $(".card-body");
 var timerEl = $(".timer-count");
 //var timerElement
 var startButton = $("#start-button");
+var scoreButton = $("#score-button").css("background-color", "blue");
 var nextButton = $(".btn-next");
 var incorrectAns = $(".false");
 var correctAns = $(".true");
+var scoreList = $("#high-scores");
+
+var scoreCounter = 0;
+var currentIndex = 0;
+
+//This stores the scores to localStorage for comparison purposes
+var storedScore = JSON.parse(localStorage.getItem("scores")) || [];
 
 $(".questions").hide();
+$(".game-over").hide();
+$(scoreButton).hide();
+$(scoreList).hide();
 
 //Selects questions to be rendered to the screen from the array
 function renderQuestion() {
@@ -37,8 +48,10 @@ function correctAnswer() {
 // The startQuiz function is called when the start button is clicked
 function startQuiz() {
   //do I need to establish something that shows the game isn't already over here?
-  timerCount = 30;
-  //Define me
+  //RESET TIMER TO 30
+  timerCount = 5;
+  // Prevents start button from being clicked during quiz (re-setting time)
+  startButton.attr("disabled", true).hide();
   startTimer();
   renderQuestion();
 }
@@ -59,12 +72,48 @@ function startTimer() {
 
 // The gameOver function is called when timer reaches 0
 function gameOver() {
-  //wordBlank.textContent = "GAME OVER";
-  //startButton.style.display = "show";
-  //Define me
-  //setScore();
+  $(".questions").hide();
+  $(".game-over").show();
+  $(startButton).hide();
+  $(scoreButton).show();
+  enterScore();
 }
 
+function renderScores() {
+  $(scoreList).show().empty();
+  if (storedScore.length === 0) {
+    scoreList.append("Please play the quiz first!");
+  } else {
+    for (var i = 0; i < storedScore.length; i++) {
+      var newLi = $("<li>");
+      newLi.text(storedScore[i].initials + " ----- " + storedScore[i].score);
+      scoreList.append(newLi);
+    }
+  }
+}
+
+function enterScore() {
+  $(scoreButton).hide();
+  $(startButton).show();
+  $(".game-over").hide();
+  var initButton = scoreList.append("button", "Save my score");
+  initButton.on("click", saveScore);
+  cardEl.text(
+    "Your score was: " +
+      scoreCounter +
+      "\n\n Click to enter score: " +
+      initButton
+  );
+  function saveScore() {
+    var scoreObject = {
+      initials: userInitials,
+      score: scoreCounter,
+    };
+    storedScore.push(scoreObj);
+    localStorage.setItem("scores", JSON.stringify(storedScore));
+  }
+  renderScores();
+}
 // **TO DO: SHOW START BUTTON AGAIN AT END OF GAME**
 //startButton.style.display = "hide";
 
@@ -72,3 +121,4 @@ function gameOver() {
 startButton.on("click", startQuiz);
 incorrectAns.on("click", incorrectAnswer);
 correctAns.on("click", correctAnswer);
+scoreButton.on("click", renderScores);
